f73d3a1126fb649c1eaa32bfdf9be36e98a64fbf
Maciek <maciej.harazinski@gmail.com>
2014-05-13 22:46:46 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videoapp::Application.config.secret_key_base = '23af116392eb1abe0fb95d45330ee5a88631854960a89bfd187140cae108db4c0cc85f4f5a95e3124f899fe736e558a87d5e957879bb238e7db53008db424726'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videoapp::Application.config.session_store :cookie_store, key: '_videoapp_session'
